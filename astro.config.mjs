29aedf7e7c4fe2081f7f3fecf652aa4134a4a9b2
digital <digital@jagapet.com>
2026-04-30 15:55:09 +0800

    1st commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       },
   },
   site: 'https://jagapet.com',
-  base: '/melayu', 
+  base: '/Melayu', 
   trailingSlash: 'always',
   outDir: 'dist',
   integrations: [sitemap(), pagefind()],
